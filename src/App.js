b0a02282970e12b26dd512edb072b18214dadc49
Montassar <elsoulimontassar@gmail.com>
2024-04-06 23:32:32 +0100

    checkpoint edit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Consulting from "./pages/Consulting";
-import Contactus from "./pages/Contactus";
-import Design from "./pages/Design";
-import Development from "./pages/Development";
-import Marketing from "./pages/Marketing";
-import Products from "./pages/Products";
-import Services from "./pages/Services";
-import Signup from "./pages/Signup";
+import { Navbar, Nav, Container, Card } from "react-bootstrap";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />}></Route>
-          <Route path="/contactus" element={<Contactus />}></Route>
-          <Route path="/products" element={<Products />}></Route>
-          <Route path="/services" element={<Services />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/marketing" element={<Marketing />}></Route>
-          <Route path="/development" element={<Development />}></Route>
-          <Route path="/design" element={<Design />}></Route>
-          <Route path="/consulting" element={<Consulting />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <React.Fragment>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">My React App</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Container className="mt-3">
+          <h1>Welcome to My React App</h1>
+          <div className="d-flex justify-content-between mt-3">
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card 1</Card.Title>
+                <Card.Text>This is the content of Card 1.</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card 2</Card.Title>
+                <Card.Text>This is the content of Card 2.</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card 3</Card.Title>
+                <Card.Text>This is the content of Card 3.</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
